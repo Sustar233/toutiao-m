75f5b847a2d42cc3bad2160f838fda425d61f4eb
sustar <3sustar@gmail.com>
2021-11-27 20:31:05 +0800

    完成频道编辑功能

---
+++ src/views/home/components/channel-edit.vue
@@ -0,0 +1,231 @@
+<template>
+  <div class="channel-edit">
+    <!-- 我的频道 -->
+    <van-cell>
+      <div slot="title">我的频道</div>
+      <van-button
+        class="edit-btn"
+        type="danger"
+        plain
+        round
+        size="small"
+        @click="isEdit = !isEdit"
+      >{{ isEdit? '完成' : '编辑' }}</van-button>
+    </van-cell>
+
+    <van-grid class="my-grid" :gutter="10">
+      <van-grid-item
+        class="grid-item"
+        v-for="(channel, index) in myChannels"
+        :key="index"
+        @click="onMyChannelClick(channel, index)"
+      >
+        <van-icon v-show="isEdit && !fiexdChannels.includes(channel.id)" slot="icon" name="clear"></van-icon>
+        <span class="text" :class="{ active: index === active }" slot="text">{{ channel.name }}</span>
+      </van-grid-item>
+    </van-grid>
+
+    <!-- 频道推荐 -->
+    <van-cell>
+      <div slot="title" class="title-text">频道推荐</div>
+    </van-cell>
+    <van-grid class="recommand-grid" :gutter="10">
+      <van-grid-item
+        class="grid-item"
+        v-for="(channel, index) in recommendChannels"
+        :key="index"
+        icon="plus"
+        :text="channel.name"
+        @click="onAddChannel(channel)"
+      />
+    </van-grid>
+  </div>
+</template>
+
+<script>
+import { 
+  getAllChannels,
+  addUserChannel,
+  deleteUserChannel
+} from '@/api/channel'
+import { mapState } from 'vuex'
+import { setItem } from '@/utils/storage'
+
+export default {
+  name: 'ChannelEdit',
+  props: {
+    myChannels: {
+      type: Array,
+      required: true
+    },
+    active: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      allChannels: [], // 所有频道
+      isEdit: false, // 控制编辑状态的显示
+      fiexdChannels: [0] // 固定频道，不允许删除
+    }
+  },
+  computed: {
+    ...mapState(['user']),
+    recommendChannels() {
+      return this.allChannels.filter(channel => {
+        return !this.myChannels.find(myChannel => {
+          return myChannel.id === channel.id
+        })
+      })
+    }
+    // recommendChannels() {
+    //   const channels = []
+    //   this.allChannels.forEach(channel => {
+    //     // find 遍历数组，找到满足条件的元素项
+    //     const ret = this.myChannels.find(myChannel => {
+    //       return myChannel.id === channel.id
+    //     })
+    //     // 如果我的频道中不包括该频道项，则收集到推荐频道中
+    //     if (!ret) {
+    //       channels.push(channel)
+    //     }
+    //   })
+    //   // 把计算结果返回
+    //   return channels
+    // }
+  },
+  methods: {
+    async loadAllChannels() {
+      try {
+        const { data } = await getAllChannels()
+        this.allChannels = data.data.channels
+      } catch (error) {
+        this.$toast('数据获取失败')
+      }
+    },
+
+    async onAddChannel(channel) {
+      this.myChannels.push(channel)
+      // 数据持久化处理
+      if (this.user) {
+        try {
+          // 已登录，把数据请求接口放到线上
+          await addUserChannel({
+            id: channel.id, // 频道ID
+            seq: this.myChannels.length// 序号
+          })
+        } catch (error) {
+          this.$toast('保存失败，请稍后重试')
+        }
+      } else {
+        // 未登录，把数据存储到本地
+        setItem('TOUTIAO_CHANNELS', this.myChannels)
+      }
+    },
+
+    onMyChannelClick(channel, index) {
+      if (this.isEdit) {
+        // 如果是固定频道，则不删除
+        if (this.fiexdChannels.includes(channel.id)) {
+          return
+        }
+        // 编辑状态执行删除
+        this.myChannels.splice(index, 1)
+
+        if (index <= this.active) {
+          // 让激活频道的索引 -1
+          this.$emit('update-active', this.active - 1, true)
+        }
+
+        // 处理持久化
+        this.deleteChannel(channel)
+      } else {
+        // 非编辑状态执行切换
+        this.$emit('update-active', index, false)
+      }
+    },
+
+    async deleteChannel(channel) {
+      try {
+        if (this.user) {
+          // 已登录, 则将数据更新到线上
+          await deleteUserChannel(channel.id)
+        } else {
+          // 未登录，将数据更新到本地
+          setItem('TOUTIAO_CHANNELS', this.myChannels)
+        }
+      } catch (error) {
+        this.$toast('操作失败，请稍后重试')
+      }
+    }
+  },
+  created() {
+    this.loadAllChannels()
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.channel-edit {
+  padding: 85px 0;
+
+  .title-text {
+    font: 32px;
+    color: #333;
+  }
+
+  .edit-btn {
+    width: 104px;
+    height: 48px;
+    font-size: 26px;
+    color: #f85959;
+    border: 1px solid #f85959;
+  }
+
+  /deep/ .grid-item {
+    width: 160px;
+    height: 86px;
+    .van-grid-item__content {
+      white-space: nowrap;
+      background-color: #f4f5f6;
+      .van-grid-item__text, .text {
+        font-size: 28px;
+        color: #222;
+        margin-top: 0;
+      }
+      .active {
+        color: red;
+      }
+      .van-grid-item__icon-wrapper {
+        position: unset;
+      }
+    }
+  }
+
+  /deep/ .my-grid {
+    .grid-item {
+      .van-icon-clear {
+        position: absolute;
+        right: -10px;
+        top: -10px;
+        font-size: 30px;
+        color: #cacaca;
+        z-index: 2;
+      }
+    }
+  }
+
+  /deep/ .recommand-grid {
+    .grid-item {
+      .van-grid-item__content {
+        flex-direction: row;
+        .van-icon-plus {
+          font-size: 28px;
+          margin-right: 6px;
+        }
+      }
+    }
+  }
+}
+</style>
