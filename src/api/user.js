21e6b956c1868fbb077ae0b2154176bad1c76440
sustar <3sustar@gmail.com>
2021-12-07 01:03:38 +0800

    test

--- src/api/user.js
+++ src/api/user.js
@@ -59,3 +59,20 @@ export const deleteFollow = target => {
     url: `/v1_0/user/followings/${target}`
   })
 }
+
+// 获取用户个人资料
+export const getUserProfile = () => {
+  return request({
+    method: 'GET',
+    url: '/v1_0/user/profile'
+  })
+}
+
+// 更新用户个人资料
+export const updateUserProfile = data => {
+  return request({
+    method: 'PATCH',
+    url: '/v1_0/user/profile',
+    data
+  })
+}
